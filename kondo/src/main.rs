5a7dd60dafd457ab1c822020912d35bc023a7d94
Trent Billington <trent.h.billington@gmail.com>
2020-04-05 18:19:57 +1000

    move no subcommand into subcommand match

--- kondo/src/main.rs
+++ kondo/src/main.rs
@@ -45,23 +45,6 @@ enum Command {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     use io::Write;
     let opt = Opt::from_args();
-    let dirs: Vec<path::PathBuf> = {
-        let cd = env::current_dir()?;
-        if opt.dirs.is_empty() {
-            vec![cd]
-        } else {
-            opt.dirs
-                .into_iter()
-                .map(|d| {
-                    if d.is_absolute() {
-                        d
-                    } else {
-                        cd.join(d).canonicalize().expect("Unable to canonicalize!")
-                    }
-                })
-                .collect()
-        }
-    };
 
     let stdout = io::stdout();
     let mut write_handle = stdout.lock();
@@ -105,65 +88,73 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 artifact_dirs.clear();
             }
             writeln!(&mut write_handle, "Disk saving: {}", pretty_size(total))?;
-            return Ok(());
+            Ok(())
         }
-        None => {}
-    }
+        None => {
+            let cd = env::current_dir()?;
+            let dirs = if opt.dirs.is_empty() {
+                vec![cd]
+            } else {
+                opt.dirs.into_iter()
+                    .map(|d| path_canonicalise(&cd, d))
+                    .collect()
+            };
+            let project_dirs: Vec<Project> = dirs.iter().flat_map(scan).collect();
 
-    let project_dirs: Vec<Project> = dirs.iter().flat_map(scan).collect();
-
-    if let Some(command) = opt.command {
-        for dir in project_dirs.iter() {
-            let dir_base = &dir.path;
-            for p in dir.artifact_dirs() {
-                let full_path = dir_base.join(p);
-                if !opt.existing_dirs || full_path.metadata().is_ok() {
-                    process::Command::new(&command).arg(full_path).spawn()?;
+            if let Some(command) = opt.command {
+                for dir in project_dirs.iter() {
+                    let dir_base = &dir.path;
+                    for p in dir.artifact_dirs() {
+                        let full_path = dir_base.join(p);
+                        if !opt.existing_dirs || full_path.metadata().is_ok() {
+                            process::Command::new(&command).arg(full_path).spawn()?;
+                        }
+                    }
                 }
-            }
-        }
-        return Ok(());
-    };
+                return Ok(());
+            };
 
-    if opt.artifact_dirs {
-        for dir in project_dirs.iter() {
-            let dir_base = &dir.path;
-            for p in dir.artifact_dirs() {
-                let full_path = dir_base.join(p);
-                if !opt.existing_dirs || full_path.metadata().is_ok() {
-                    writeln!(&mut write_handle, "{}", full_path.to_string_lossy())?;
+            if opt.artifact_dirs {
+                for dir in project_dirs.iter() {
+                    let dir_base = &dir.path;
+                    for p in dir.artifact_dirs() {
+                        let full_path = dir_base.join(p);
+                        if !opt.existing_dirs || full_path.metadata().is_ok() {
+                            writeln!(&mut write_handle, "{}", full_path.to_string_lossy())?;
+                        }
+                    }
                 }
+                return Ok(());
             }
+
+            let mut total = 0;
+
+            let mut project_sizes: Vec<(u64, String, &str)> = project_dirs
+                .iter()
+                .flat_map(|p| match p.size() {
+                    0 => None,
+                    size => {
+                        total += size;
+                        Some((size, p.name(), p.type_name()))
+                    }
+                })
+                .collect();
+
+            project_sizes.sort_unstable_by_key(|p| p.0);
+
+            for (size, name, type_name) in project_sizes.iter() {
+                writeln!(
+                    &mut write_handle,
+                    "{:>10} {:<6} {}",
+                    pretty_size(*size),
+                    type_name,
+                    name
+                )?;
+            }
+
+            writeln!(&mut write_handle, "{} possible savings", pretty_size(total))?;
+
+            Ok(())
         }
-        return Ok(());
     }
-
-    let mut total = 0;
-
-    let mut project_sizes: Vec<(u64, String, &str)> = project_dirs
-        .iter()
-        .flat_map(|p| match p.size() {
-            0 => None,
-            size => {
-                total += size;
-                Some((size, p.name(), p.type_name()))
-            }
-        })
-        .collect();
-
-    project_sizes.sort_unstable_by_key(|p| p.0);
-
-    for (size, name, type_name) in project_sizes.iter() {
-        writeln!(
-            &mut write_handle,
-            "{:>10} {:<6} {}",
-            pretty_size(*size),
-            type_name,
-            name
-        )?;
-    }
-
-    writeln!(&mut write_handle, "{} possible savings", pretty_size(total))?;
-
-    Ok(())
 }
