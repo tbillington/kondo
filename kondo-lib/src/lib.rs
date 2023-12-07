05f0600a628bb3a78576755f6de0e1d6c90c1a85
John T. Wodder II <jwodder@users.noreply.github.com>
2023-12-07 09:06:30 -0500

    Add `.tox` to `PROJECT_PYTHON_DIRS` (#104)

--- kondo-lib/src/lib.rs
+++ kondo-lib/src/lib.rs
@@ -49,11 +49,12 @@ const PROJECT_UNREAL_DIRS: [&str; 5] = [
     "Intermediate",
 ];
 const PROJECT_JUPYTER_DIRS: [&str; 1] = [".ipynb_checkpoints"];
-const PROJECT_PYTHON_DIRS: [&str; 7] = [
+const PROJECT_PYTHON_DIRS: [&str; 8] = [
     ".mypy_cache",
     ".nox",
     ".pytest_cache",
     ".ruff_cache",
+    ".tox",
     ".venv",
     "__pycache__",
     "__pypackages__",
